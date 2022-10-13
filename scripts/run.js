85688a1a88c45f2d753acc171be31dfb360e13cf
abu kausar <abukausar33@gmail.com>
2022-10-13 21:03:22 +0600

    all files added

---
+++ scripts/run.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat")
+
+const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed to:", owner.address);
+
+    await waveContract.getTotalWaves();
+
+    const firstWaveTxn = await waveContract.wave();
+    await firstWaveTxn.wait();
+
+    await waveContract.getTotalWaves();
+
+    const secondWaveTxn = await waveContract.wave();
+    await secondWaveTxn.wait();
+
+    await waveContract.getTotalWaves();
+};
+
+const runMain = async () => {
+    try{
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
